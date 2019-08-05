dd71eedf9799ff9e4f1567135aa61c5db56d0282
m7r-227 <msetair.dedeset@gmail.com>
2019-08-05 13:34:00 +0200

    adding qualities menu

--- src/index.js
+++ src/index.js
@@ -1,33 +1,63 @@
 const dashjs = require('dashjs');
+/**
+ * @type { videojs.default }
+ */
 const videojs = require('video.js');
 
-require('silvermine-videojs-quality-selector')(videojs);
-
-const player = dashjs.MediaPlayer().create();
+const dashPlayer = dashjs.MediaPlayer().create();
 const video = document.querySelector('#videoplayer');
 const url = 'https://dash.akamaized.net/envivio/EnvivioDash3/manifest.mpd';
 
-player.initialize(video, url, false);
-player.updateSettings({ streaming: { abr: { autoSwitchBitrate: false } } });
-player.on('streamInitialized', () => {
-    const qualities = player.getBitrateInfoListFor('video');
-    const qualityIndex = qualities[4].qualityIndex; // lowest
-    // const qualityIndex = qualities[qualities.length - 1].qualityIndex; // highest
-    player.setQualityFor('video', qualityIndex);
-});
+dashPlayer.initialize(video, url, false);
+dashPlayer.updateSettings({ streaming: { abr: { autoSwitchBitrate: false } } });
+dashPlayer.on('streamInitialized', () => {
+    const qualities = dashPlayer.getBitrateInfoListFor('video');
+    const qualityIndex = qualities[0].qualityIndex; // lowest
+    // // const qualityIndex = qualities[qualities.length - 1].qualityIndex; // highest
+    dashPlayer.setQualityFor('video', qualityIndex);
 
-const videojsOptions = {
-    controls: true,
-    autoplay: false,
-    controlBar: {
-        children: [
-            'playToggle',
-            'progressControl',
-            'volumePanel',
-            'qualitySelector',
-            'fullscreenToggle',
-        ]
+    const videojsOptions = {
+        controls: true,
+        autoplay: true,
+        controlBar: {
+            children: [
+                'playToggle',
+                'progressControl',
+                'volumePanel',
+                'qualitySelector',
+                'fullscreenToggle',
+            ]
+        }
+    };
+
+    const vjsPlayer = videojs(video, videojsOptions);
+    const MenuButton = videojs.getComponent('MenuButton');
+    const menuButton = new MenuButton(vjsPlayer);
+
+    const Menu = videojs.getComponent('Menu');
+    const menu = new Menu(vjsPlayer, { menuButton });
+
+    const MenuItem = videojs.getComponent('MenuItem');
+
+    const QualityMenuItem = videojs.extend(MenuItem, {
+        constructor: function (player, options) {
+            this.qualityIndex = options.qualityIndex;
+            MenuItem.call(this, player, options);
+        },
+
+        handleClick: function (event) {
+            MenuItem.prototype.handleClick.call(this, event);
+            console.log(this.qualityIndex);
+            dashPlayer.setQualityFor('video', this.qualityIndex);
+        }
+    });
+
+    for (const quality of qualities) {
+        const menuItem1 = new QualityMenuItem(vjsPlayer, { label: quality.bitrate, qualityIndex: quality.qualityIndex });
+        menu.addItem(menuItem1);
     }
-};
 
-const v = videojs(video, videojsOptions);
+    menuButton.addChild(menu);
+
+    vjsPlayer.controlBar.addChild(menuButton);
+});
